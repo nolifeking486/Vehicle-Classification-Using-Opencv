55d5bfc058fd77ea76d16e08e773fb5248dd4820
DEVANSH SHARMA <83576606+nolifeking486@users.noreply.github.com>
2021-07-09 01:03:52 +0530

    added files for yolo training

---
+++ yolov4/generate_test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  8 15:02:07 2021
+
+@author: devan
+"""
+
+import os
+
+image_files = []
+os.chdir(os.path.join("data", "test"))
+for filename in os.listdir(os.getcwd()):
+    if filename.endswith(".jpg"):
+        image_files.append("data/test/" + filename)
+os.chdir("..")
+with open("test.txt", "w") as outfile:
+    for image in image_files:
+        outfile.write(image)
+        outfile.write("\n")
+    outfile.close()
+os.chdir("..")
---
+++ yolov4/generate_train.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  8 15:02:42 2021
+
+@author: devan
+"""
+
+import os
+
+image_files = []
+os.chdir(os.path.join("data", "obj"))
+for filename in os.listdir(os.getcwd()):
+    if filename.endswith(".jpg"):
+        image_files.append("data/obj/" + filename)
+os.chdir("..")
+with open("train.txt", "w") as outfile:
+    for image in image_files:
+        outfile.write(image)
+        outfile.write("\n")
+    outfile.close()
+os.chdir("..")
