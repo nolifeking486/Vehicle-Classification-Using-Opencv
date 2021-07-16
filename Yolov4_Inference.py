f70a4f96dd60de05738c46b6a9e22defd52a79b8
DEVANSH SHARMA <83576606+nolifeking486@users.noreply.github.com>
2021-07-16 17:43:23 +0530

    Create Yolov4_Inference.py

---
+++ Yolov4_Inference.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import cv2 as cv
+import time
+
+Conf_threshold = 0.4
+NMS_threshold = 0.4
+COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+
+
+class_name = []
+with open('C:\\Users\\devan\\OneDrive\\Documents\\GitHub\\Vehicle-Classification-Using-Opencv\\yolov4\\classes.txt', 'r') as f:
+    class_name = [cname.strip() for cname in f.readlines()]
+#print(class_name)
+
+net = cv.dnn.readNet('C:\\Users\\devan\\OneDrive\\Documents\\GitHub\\Vehicle-Classification-Using-Opencv\\custom_model_weights\\yolov4-obj_bestv6.weights', 'C:\\Users\\devan\\OneDrive\\Documents\\GitHub\\Vehicle-Classification-Using-Opencv\\yolov4\\yolov4-obj.cfg')
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA_FP16)
+
+model = cv.dnn_DetectionModel(net)
+model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
+
+
+cap = cv.VideoCapture('C:\\Users\\devan\\Downloads\\traffictest4.mp4')
+starting_time = time.time()
+frame_counter = 0
+while True:
+    ret, frame = cap.read()
+    frame_counter += 1
+    if ret == False:
+        break
+    classes, scores, boxes = model.detect(frame, Conf_threshold, NMS_threshold)
+    for (classid, score, box) in zip(classes, scores, boxes):
+        color = COLORS[int(classid) % len(COLORS)]
+        label = "%s : %f" % (class_name[classid[0]], score)
+        cv.rectangle(frame, box, color, 1)
+        cv.putText(frame, label, (box[0], box[1]-10),
+                   cv.FONT_HERSHEY_COMPLEX, 0.3, color, 1)
+    endingTime = time.time() - starting_time
+    fps = frame_counter/endingTime
+    # print(fps)
+    cv.putText(frame, f'FPS: {fps}', (20, 50),
+               cv.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+    cv.imshow('frame', frame)
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
